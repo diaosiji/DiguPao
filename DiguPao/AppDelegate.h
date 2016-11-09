151849c1a3f031ff0c73e78a37af9993ca3dd07b
屌斯基 <diaosiji@diaosijideMacBook-Pro.local>
2016-11-09 08:55:08 +0800

    Initial Commit

---
+++ DiguPao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiguPao
+//
+//  Created by 屌斯基 on 2016/11/9.
+//  Copyright © 2016年 intelligentunit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
