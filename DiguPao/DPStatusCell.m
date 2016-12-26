6e7519d45c1eae3454390d1aab24f84732f0be12
songrotek <songrotek@qq.com>
2016-12-26 11:12:41 +0800

    fix bug

--- DiguPao/DPStatusCell.m
+++ DiguPao/DPStatusCell.m
@@ -194,9 +194,6 @@
     // Configure the view for the selected state
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"touch status cell" object:self.statusFrame];
-}
+
 
 @end
